09edf474980595f3b7f3f2c09c0209ea1df730b9
Kushanyasiru97 <kushanyasiruwalpola@gmail.com>
2024-09-10 02:46:46 +0530

    Basic Navbar component Created

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+import DesktopLogo from "../../public/airbnb-desktop.png";
+import MobileLogo from "../../public/airbnb-mobile.webp";
+import { UserNav } from "./UserNav";
+
+export function Navbar() {
+  return (
+    <nav className="w-full border-b">
+      <div className="flex items-center justify-between container mx-auto lg:px-10 py-5">
+        <Link href="/" passHref>
+          <div>
+            <Image 
+              src={DesktopLogo} 
+              alt="Desktop Logo" 
+              className="hidden lg:block" 
+              width={128} 
+              height={32}
+            />
+            <Image 
+              src={MobileLogo} 
+              alt="Mobile Logo" 
+              className="block lg:hidden" 
+              width={48} 
+              height={48}
+            />
+          </div>
+        </Link>
+        <div className="rounded-full border px-5 py-2">
+          <h1>Hello From the Search</h1>
+        </div>
+        <UserNav />
+      </div>
+    </nav>
+  );
+}
---
+++ app/components/UserNav.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @next/next/no-img-element */
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { MenuIcon } from "lucide-react";
+import {RegisterLink, LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export async function UserNav(){
+
+    const {getUser} =getKindeServerSession();
+    const user = await getUser();
+
+    return(
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+            <div className="rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
+            <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" />
+            <img src={user?.picture?? "https://img.freepik.com/free-icon/user_318-804790.jpg"} alt="User Image" className="rounded-full h-8 w-8 hidden lg:block"/>
+            </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-[200px]">
+                {user ? (
+                    <>
+                 <DropdownMenuItem>
+                     <LogoutLink className="w-full">Log Out</LogoutLink>
+                 </DropdownMenuItem>
+                    </> 
+                ): (
+                   <>
+                   <DropdownMenuItem>
+                    <RegisterLink className="w-full">Register</RegisterLink>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <LoginLink className="w-full">Login</LoginLink>
+                </DropdownMenuItem>
+                   </> 
+                )}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
